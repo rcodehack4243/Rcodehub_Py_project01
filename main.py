91d23b7f58062ccadae2cd3c4097daa38e741253
rcode4243 <104592882+rcodehack4243@users.noreply.github.com>
2022-05-16 11:51:06 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+print('hellow python')
+"""
+this 
+is 
+cokments 
+"""
+
+name = input("input your name : ")
+email = input("input your email : ")
+
+
+# if 4>5:
+#     print("python if blck")
+# else:
+#     print('python else block')
+# for x in range(6):
+#   print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+ 
+print(f"Name: {name}\nEmail: {email} ")
